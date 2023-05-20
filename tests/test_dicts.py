7c186480c45613b24357aa1fb916185787c1f01b
Filin Mikhail <filin.md9@gmail.com>
2023-05-21 01:14:55 +0300

    add dicts with func get val and test

---
+++ tests/test_dicts.py
@@ -0,0 +1,7 @@
+from utils.dicts import get_val
+
+
+def test_get_val():
+    assert get_val({"vcs": "mercurial"}, "vcs") == "mercurial"
+    assert get_val({"vcs": "mercurial"}, "vcs", "git") == "mercurial"
+    assert get_val({}, "vcs", "bazaar") == "bazaar"
